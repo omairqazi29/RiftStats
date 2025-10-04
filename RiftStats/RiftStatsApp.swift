38422a833b80e0419598725b6f7fc61e4ba29909
Q. Omair Ahmed <me@omairahmed.me>
2025-10-04 14:23:03 -0400

    Initial Commit

---
+++ RiftStats/RiftStatsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RiftStatsApp.swift
+//  RiftStats
+//
+//  Created by Omair Ahmed on 10/4/25.
+//
+
+import SwiftUI
+
+@main
+struct RiftStatsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
